77b46975ff765cc885ee6f341c0c22f6ebd7e1a8
Peter Sundström <peter.sundstrom@modelon.com>
2023-12-10 19:40:48 +0000

    day 10 part 2 wip

--- 2023/Day10/day10.py
+++ 2023/Day10/day10.py
@@ -30,6 +30,13 @@ pipes={
     '.': {},
 }
 
+right={
+    (1,0):(0,-1),
+    (-1,0):(0,1),
+    (0,1):(1,0),
+    (0,-1):(-1,0),
+}
+
 def printposition(x,y):
     for xx in [-1,0,1]:
         p = ''
@@ -37,15 +44,14 @@ def printposition(x,y):
             p+=lines[x+xx][y+yy]
         print(p)
 
+def printall(lines,tiles,enclosed):
+    for x in range(len(lines)):
+        g = ''
+        for y in range(len(lines[0])):
+            g+='O' if (x,y) in enclosed else 'x' if (x,y) in tiles else lines[x][y]
+        print(g)
+
 printposition(startx,starty)
-# for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-# # for dx in [-1,0,1]:
-# #     # p = ''
-# #     for dy in [-1,0,1]:
-#     v = lines[startx+dx][starty+dy] 
-#     # p+=lines[startx+dx][starty+dy]
-#     if (dx,dy) in pipes[v].keys():
-#         print((dx,dy),v)
 
 x=start[0]
 y=start[1]
@@ -56,15 +62,18 @@ visited=[(x,y)]
 dist[(x,y)]=0
 print('At',x,y)
 d=0
-
+ch=[]
 for dx,dy in [(0,1),(-1,0)]:
+# for dx,dy in [(0,-1),(1,0)]:
+    R=dx==0
     x=start[0]
     y=start[1]
     d=0
+    enclosed=set()
     while True:
-        # printposition(x,y)
-        # x_=x
-        # y_=y
+        if R:
+            dx_,dy_ = right[(dx,dy)]
+            ch.append((x+dx_,y+dy_))
         x=x+dx
         y=y+dy
         d+=1
@@ -72,70 +81,66 @@ for dx,dy in [(0,1),(-1,0)]:
             break
         if (x,y) in dist.keys():
             dist[(x,y)] = min(dist[(x,y)],d)
-            # print('redist',x,y,lines[x][y],dist[(x,y)],d)
         else:
-            # if (x,y) in dist.keys():
-            #     print(d,dist[(x,y)])
-            # print('dist',x,y,lines[x][y],d)
             dist[(x,y)]=d
         if lines[x][y]=='.':
             print('Fail')
             break
-        # print('At',lines[x][y],dx,dy)
         visited.append((x,y))
         if x<0 or x>=len(lines):
             print('Out of bounds x',x)
             break
-
         dx,dy = pipes[lines[x][y]][(dx,dy)]
 
-        # for xx in [-1,0,1]:
-        #     for yy in [-1,0,1]:
-        # for xx,yy in [(1,0),(-1,0),(0,1),(0,-1)]:
-        #     if 0<=x+xx<len(lines)-1 and 0<=y+yy<len(lines[0])-1:
-        #         v = lines[x+xx][y+yy]
-        #         if (x+xx,y+yy) not in visited and (dx,dy) in pipes[v].keys():
-        #             print(x,y,(xx,yy),v)
-        #             dx=xx
-        #             dy=yy
+print(len(dist.keys()))
 
-#   1. Set Q to the empty queue or stack.
-#   2. Add node to the end of Q.
-#   3. While Q is not empty:
-#   4.   Set n equal to the first element of Q.
-#   5.   Remove first element from Q.
-#   6.   If n is Inside:
-#          Set the n
-#          Add the node to the west of n to the end of Q.
-#          Add the node to the east of n to the end of Q.
-#          Add the node to the north of n to the end of Q.
-#          Add the node to the south of n to the end of Q.
-#   7. Continue looping until Q is exhausted.
-#   8. Return.
+def findenclosed(x0,y0,tiles):
+    if (x0,y0) in tiles:
+        return set([])
+    # print(x0,y0)
+    Q=[(x0,y0)]
+    contained=set()
+    while len(Q)>0:
+        x,y=Q.pop()
+        # print(x,y)
+        if x<0 or x>len(lines) or y<0 or y>len(lines[0]):
+            # print('BLA----',len(contained))
+            return set([]) # Nothing enclosed if we reach the outside edge
+        elif (x,y) not in tiles:
+            # print('enclosed tile')
+            contained.add((x,y))
+            # print(contained)
+        for xx,yy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                if (x+xx,y+yy) not in tiles and (x+xx,y+yy) not in contained:
+                    Q.append((x+xx,y+yy))
+    # if len(contained)==1:
+    #     for item in contained:
+    #         printposition(*item)
+    # print('HALLOOO',len(contained))
+    return contained
 
-enclosed={}
-tiles = [k for k in dist.keys()]
-for x0 in range(len(lines)):
-    for y0 in range(len(lines[0])):
-        if (x0,y0) in tiles:
-            enclosed[(x0,y0)]=False
-            break
-        # enclosed[(x0,y0)]=True
-        Q = [(x0,y0)]
-        x,y = Q.pop()
-        if (x,y) in tiles:
-            pass
-        elif x<0 or x>len(lines) or y<0 or y>len(lines[0]):
-            enclosed[(x0,y0)]=False
-        else:
-            for xx,yy in [(1,0),(-1,0),(0,1),(0,-1)]:
-                Q.append((x+xx,y+yy))
+tiles=[k for k in dist.keys()]
+# print(ch)
+print(len(ch))
 
-print(len(enclosed),len(lines)*len(lines[0]))
+print(len([lines[x][y] for x,y in dist.keys() if lines[x][y] in ['-','|']]))
 
-print(sum([1 for key in enclosed.keys() if enclosed[key]]))
+# assert(False)
+enclosed=set()
+for x,y in ch:
+    if (x,y) not in enclosed:
+        # print('check',x,y)
+        enclosed.update(findenclosed(x,y,tiles))
 
-print(enclosed)
+
+printall(lines,[],[])
+print('-')
+printall(lines,tiles,enclosed)
+print('-')
+printall(lines,tiles,ch)
+print('-')
+# print(enclosed)
+print(len(enclosed))
 
 print('------------------------')
 print('Part 1:',max(dist.values()))

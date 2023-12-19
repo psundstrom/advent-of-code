1d4378826c64b4c835ddc627722c0cdeb9b35500
peter.sundstrom <peter.sundstrom@modelon.com>
2023-12-19 08:37:04 +0100

    day 19 part 2 wip

--- 2023/Day19/day19.py
+++ 2023/Day19/day19.py
@@ -60,7 +60,23 @@ def applyrule(part,rule):
     else:
         assert False    
 
+def applyrule2(part,rule):
+    # part is ranges [(low,high),(low,high),(low,high),(low,high)]
+    # return new parts with ranges with their corresponding targets
+    
+    # return list of parts and targets...?
+    
+    return 0
 
+def applyworkflow2(part,w):
+    # part is ranges [(low,high),(low,high),(low,high),(low,high)]
+    workflow = W[w]
+    parts=[part]
+    targets=[w]
+    for part,target in zip(parts,targets):
+        for rule in workflow:
+            parts,targets=applyrule2(part,rule)
+    return 0
 
 def applyworkflow(part,w):
     workflow = W[w]
@@ -78,6 +94,17 @@ for part in P:
     result=applyworkflow(part,'in')
     if result=='A':
         A.append(part)
+
+# in{
+#     part=[(0,4000),(0,4000),(0,4000),(0,4000)]
+
+#     m>1770:pm -> m=1771-4000 goes to pm
+#    ,m>644:sz, -> m=645-1770 goes to sz
+#     nc} m=0-644 goes to nc
+
+#     applyworkflow([(1771,4000),(0,4000),(0,4000),(0,4000)],'pm')
+#     applyworkflow([(645,1770),(0,4000),(0,4000),(0,4000)],'sz')
+#     applyworkflow([(0,644),(0,4000),(0,4000),(0,4000)],'nc')
 
 print('------------------------')
 print('Part 1:',sum([sum(p) for p in A]))

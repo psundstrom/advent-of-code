ed7c06868dcd37cc810f49e0b6d5a2f4f77ec9e2
peter.sundstrom <peter.sundstrom@modelon.com>
2023-12-21 20:48:07 +0100

    day 17, works on example, too slow for real input

--- 2023/Day17/day17.py
+++ 2023/Day17/day17.py
@@ -13,22 +13,23 @@ def yellow(str):
 with open('./2023/Day17/input.ex') as file:
     M = [[int(n) for n in line.rstrip()] for line in file]
 
-H=[]
-for row in M:
-    print(row)
-    H.append([100000]*len(row))
+H={}
+# for row in M:
+#     print(row)
+#     H.append([100000]*len(row))
 R=len(M)
 C=len(M[0])
 
 Q = []
-PREV = {}
+# PREV = {}
 
 start=((0,0),)
 goal=(R-1,C-1)
 
 SEEN={start}
+# PATHS={start}
 Q.append(start)
-H[0][0]=0
+H[((0,0),)]=0
 
 D={
     (1,0): [(1,0), (0,-1), (0,1)], 
@@ -36,10 +37,6 @@ D={
     (0,1): [(0,1), (-1,0), (1,0)],
     (0,-1): [(0,-1), (-1,0), (1,0)],
 }
-
-def value(p):
-    print('p:',p)
-    return H[p[0][0]][p[0][1]]
 
 def printq(current):
     # c is ((),(),(),())
@@ -75,21 +72,26 @@ def printmap():
 # add neighbours as usual, for each state in queue
 # Set tentative as usual
 
-print(Q)
+def getcost(path):
+    ans=0
+    for r,c in path[:-1]:
+        ans+=M[r][c]
+    return ans
+
 while Q:
-    print(Q)
-    Q.sort(key=value)
+    Q.sort(key=lambda x:H[x])
     current = Q.pop(0)
-    if (current[0][0],current[0][1])==goal:
+    if current[0]==goal:
         break
     r,c = current[0]
+
     nod=[]
     if len(current)>1:
         nod.append((current[1][0]-current[0][0],current[1][1]-current[0][1]))
     if len(current)>3:
         # Check if all four is on a row or column
-        rh = set([r for r,c in current])
-        ch = set([c for r,c in current])
+        rh = set([r for r,c in current[:4]])
+        ch = set([c for r,c in current[:4]])
         if len(rh)==1:
             nod.extend([(0,1),(0,-1)])
         elif len(ch)==1:
@@ -97,38 +99,26 @@ while Q:
     for dr,dc in [(dr,dc) for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)] if (dr,dc) not in nod]:
         rr=r+dr
         cc=c+dc
-        if 0<=rr<R and 0<=cc<C and ((rr,cc),)+current[1:] not in SEEN:
-            tentative = H[r][c]+M[rr][cc]
-            if tentative<H[rr][cc]:
-                PREV[(rr,cc)]=(r,c)
-                H[rr][cc]=tentative
+        if 0<=rr<R and 0<=cc<C and ((rr,cc),)+current[:3] not in SEEN: #(rr,cc) not in [c[0] for c in SEEN]:
+            tentative = H[current]+M[rr][cc]
+            if ((rr,cc),)+current[:3] not in H.keys() or tentative<H[((rr,cc),)+current[:3]]:
+                H[((rr,cc),)+current[:3]]=tentative
                 Q.append(((rr,cc),)+current[:3])
-    SEEN.add(current)
-    printmap()
+    
+    # PATHS.add(current)
+    SEEN.add(current[:4])
+
     if len(Q)==0:
         print('fail')
 
-print(H)
-print(H[R-1][C-1])
-# print(PREV)
+# for path in PATHS:
+#     if path[0]==goal:
+#         # print(H[path])
+#         print(getcost(path),path)
+#         if getcost(path)==101:
+#             print(getcost(path))
 
-next=(R-1,C-1)
-PATH=[next]
-while next!=(0,0):
-    PATH.append(PREV[next])
-    next=PREV[next]
-
-for r,row in enumerate(M):
-    g=''
-    for c,n in enumerate(row):
-        if (r,c) in PATH:
-            g+="\033[92m#\033[00m"
-        elif (r,c) not in SEEN:
-            g+=f"\033[91m{n}\033[00m"
-        else:
-            g+=str(n)
-    print(g)
-
+print(min([H[key] for key in H.keys() if key[0]==goal]))
 
 print('------------------------')
 print('Part 1:',0)

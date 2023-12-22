1fdcdd4b812f4e0e97803bf4f727a9c3e31909f1
peter.sundstrom <peter.sundstrom@modelon.com>
2023-12-22 10:46:22 +0100

    day22 falling works

--- 2023/Day22/day22.py
+++ 2023/Day22/day22.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import plotly.graph_objects as go
 
 print('2023 - Day 22')
 
@@ -11,7 +12,7 @@ F=[]
 for line in lines:
     brick=[[int(p) for p in a.split(',')] for a in line.split('~')]
     print(brick)
-    if 1 in brick[0] or 1 in brick[1]:
+    if brick[0][2]==1 or brick[1][2]==1:
         R.append(brick)
     else:
         F.append(brick)
@@ -20,25 +21,26 @@ print(B)
 
 def testcollision(bricks):
     # Test if two bricks occupy the same space
-    p=[[[],[],[]],[[],[],[]]]
+    p=[[],[]] # list of points for each brick
     for j,brick in enumerate(bricks):
         for i in 0,1,2:
             if brick[0][i]!=brick[1][i]:
                 for n in range(min(brick[0][i],brick[1][i]),max(brick[0][i],brick[1][i])+1):
-                    p[j][i].append(n)
+                    point=[0,0,0]
+                    point[i]=n
                     for ix in [ix for ix in [0,1,2] if ix!=i]:
-                        p[j][ix].append(brick[0][ix])
+                        point[ix]=brick[0][ix]
+                    p[j].append(tuple(point))
     
     collision=False
-    for i,coord in enumerate(p[0]):
-        for c in coord:
-            if c in p[1][i]:
-                collision=True
+    for point in p[0]:
+        if point in p[1]:
+            collision=True
     return collision
 
 def fall():
     F.sort(key=lambda x:min(x[0][2],x[1][2])) # Lowest z coordinate first
-    for _ in range(len(F)):
+    for bn in range(len(F)):
         brick = F.pop(0)
         brick_ = deepcopy(brick)
         brick_[0][2]-=1
@@ -49,7 +51,10 @@ def fall():
                 fcollision=True
         rcollision=False
         for brick2 in R:
-            if testcollision([brick_,brick2]) or brick_[0][2]==0 or brick_[1][2]==0:
+            if brick_[0][2]==0 or brick_[1][2]==0:
+                rcollision=True
+                break
+            if testcollision([brick_,brick2]):
                 rcollision=True
         if rcollision:
             R.append(brick)
@@ -67,6 +72,17 @@ while len(F)>0:
     print(R)
 
 print('fallen')
+
+fig = go.Figure()
+
+for i,b in enumerate(R):
+    print(b)
+    x=[b[0][0],b[1][0]]
+    y=[b[0][1],b[1][1]]
+    z=[b[0][2],b[1][2]]
+    fig.add_trace(go.Scatter3d(x=x,y=y,z=z,name=i))
+
+fig.show()
 
 print('------------------------')
 print('Part 1:',0)

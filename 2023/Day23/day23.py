69d577c94d7bb9ee8871b3a578e37a6a420176dc
Peter Sundström <peter.sundstrom@modelon.com>
2023-12-23 20:02:19 +0000

    day 23 update

--- 2023/Day23/day23.py
+++ 2023/Day23/day23.py
@@ -1,5 +1,12 @@
 from collections import deque, defaultdict
 
+def green(str):
+    return f"\033[92m{str}\033[00m"
+def red(str):
+    return f"\033[91m{str}\033[00m"
+def yellow(str):
+    return f"\u001b[33m{str}\033[00m"
+
 print('2023 - Day 23')
 
 with open('./2023/Day23/input.txt') as file:
@@ -22,20 +29,41 @@ SEEN=set()
 DIST=defaultdict(lambda:-1) #=0?
 DIST[start]=0
 D={'.':[(1,0),(-1,0),(0,1),(0,-1)],'<':[(0,-1)],'>':[(0,1)],'v':[(1,0)],'^':[(-1,0)]}
+PATHS=[]
+part2=True
 while Q:
     # Q.sort(key=lambda x:DIST[x])
     current=Q.popleft()
     r,c=current[0]
     if (r,c) == goal:
+        PATHS.append(current)
         print(DIST[(r,c)])
-    for dr,dc in D['.']: #M[r][c]]:
+    for dr,dc in D['.' if part2 else M[r][c]]:
         rr=r+dr
         cc=c+dc
         if 0<=rr<R and 0<=cc<C and (rr,cc) not in current and M[rr][cc]!='#':
             if DIST[(r,c)]+1>DIST[(rr,cc)]:
                 DIST[(rr,cc)]=DIST[(r,c)]+1
                 Q.append(((rr,cc),)+current)
-    SEEN.add((r,c))
+    SEEN.add(current)
+
+ans = max([len(path)-1 for path in PATHS])
+print(ans)
+
+for path in PATHS:
+    if len(path)-1==ans:
+        longest=path
+
+print(longest[0],goal)
+
+for r,row in enumerate(M):
+    g=''
+    for c,ch in enumerate(row):
+        if (r,c) in longest:
+            g+=green('O')
+        else:
+            g+=ch
+    print(g)
 
 # print(SEEN)
 # print(DIST[goal])

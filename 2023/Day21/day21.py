db6276dcc1d26aeaf07e9500ee784206a6dc432a
Peter Sundström <peter.sundstrom@modelon.com>
2023-12-25 07:42:49 +0000

    day 21 updates

--- 2023/Day21/day21.py
+++ 2023/Day21/day21.py
@@ -1,10 +1,18 @@
 print('2023 - Day 21')
 
+def green(str):
+    return f"\033[92m{str}\033[00m"
+def red(str):
+    return f"\033[91m{str}\033[00m"
+def yellow(str):
+    return f"\u001b[33m{str}\033[00m"
+
 with open('./2023/Day21/input.ex') as file:
     lines = [line.rstrip() for line in file]
 
 M=[]
 for r,line in enumerate(lines):
+    print(line)
     M.append(line)
     if 'S' in line:
         start=(r,line.find('S'))
@@ -26,16 +34,18 @@ for _ in range(50):
     for r,c in NEXT:
         for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
             rr,cc = r+dr,c+dc
-            if M[r%R][c%C]!='#':
+            if M[rr%R][cc%C]!='#':
                 NEW.add((rr,cc))
 
     NEXT=NEW
 
-for r in range(-3*R,1*R):
+# How many steps to fill one original square?
+
+for r in range(-3*R,3*R):
     g=''
-    for c in range(-1*C,2*C):
+    for c in range(-3*C,3*C):
         if (r,c) in NEXT:
-            g+='O'
+            g+=green('O')
         else:
             g+=M[r%R][c%C]
     print(g)

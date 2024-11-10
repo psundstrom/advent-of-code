7d1a262ee3ce0ab6176d283769df4ee090bb7d00
Peter <peter.sundstrom@hey.com>
2024-11-10 21:56:30 +0100

    rewritten based on hyperneutrinos solution

--- 2023/Day17/day17.py
+++ 2023/Day17/day17.py
@@ -1,6 +1,6 @@
 print('2023 - Day 17')
 
-from collections import deque
+from heapq import heappush, heappop
 
 def green(str):
     return f"\033[92m{str}\033[00m"
@@ -9,131 +9,80 @@ def red(str):
 def yellow(str):
     return f"\u001b[33m{str}\033[00m"
 
-
 with open('./2023/Day17/input.txt') as file:
     M = [[int(n) for n in line.rstrip()] for line in file]
 
-H={}
-# for row in M:
-#     print(row)
-#     H.append([100000]*len(row))
 R=len(M)
 C=len(M[0])
 
+SEEN=set()
 Q = []
 # PREV = {}
 
-start=((0,0),)
-goal=(R-1,C-1)
-
-SEEN={start}
-# PATHS={start}
-Q.append(start)
-H[((0,0),)]=0
-
-D={
-    (1,0): [(1,0), (0,-1), (0,1)], 
-    (-1,0): [(-1,0), (0,-1), (0,1)],
-    (0,1): [(0,1), (-1,0), (1,0)],
-    (0,-1): [(0,-1), (-1,0), (1,0)],
-}
-
-def printq(current):
-    # c is ((),(),(),())
-    for r,row in enumerate(M):
-        g=''
-        for c,n in enumerate(row):
-            if (r,c)==current[0]:
-                g+=green(str(n))
-            elif (r,c) in current[1:]:
-                g+=yellow('o')
-            else:
-                g+=str(n)
-        print(g)
-    
-
-def printmap():
-    for r,row in enumerate(M):
-        g=''
-        for c,n in enumerate(row):
-            if (r,c) in [c[0] for c in SEEN]:
-                g+=green(str(n))
-            elif (r,c) in [c[0] for c in Q]:
-                g+=yellow('o')
-            else:
-                g+=str(n)
-        print(g)
-
-# Keep track of travelled paths
-# State = current,current-1,current-2,current-3
-# Add (current,current-1,current-2,current-3) to Q
-# current is the node we are investigating
-# check previous directions to know what positions are allowed
-# add neighbours as usual, for each state in queue
-# Set tentative as usual
-
-def getcost(path):
-    ans=0
-    for r,c in path[:-1]:
-        ans+=M[r][c]
-    return ans
+Q.append((0, 0, 0, 0, 0, 0))
 
 while Q:
-    # Q.sort(key=lambda x:H[x])
-    hl=[H[x] for x in Q]
-    current = Q.pop(hl.index(min(hl)))
-    if current[0]==goal:
+    hl, r, c, dr, dc, n = heappop(Q)
+    
+    if r==R-1 and c==C-1:
         break
-    r,c = current[0]
-    nod=[]
-    currentdir=(0,0)
-    if len(current)>1:
-        currentdir = (current[0][0]-current[1][0],current[0][1]-current[1][1])
-        nod.append((current[1][0]-current[0][0],current[1][1]-current[0][1]))
-    if len(current)>10:
-        # Check if all four is on a row or column
-        rh = set([r for r,c in current[:11]])
-        ch = set([c for r,c in current[:11]])
-        if len(rh)==1:
-            nod.extend([(0,1),(0,-1)])
-        elif len(ch)==1:
-            nod.extend([(1,0),(-1,0)])
-    for dr,dc in [(dr,dc) for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)] if (dr,dc) not in nod]:
-        
-        if (dr,dc)!=currentdir:
-            tc = ((r+4*dr,c+4*dc),(r+3*dr,c+3*dc),(r+2*dr,c+2*dc),(r+1*dr,c+1*dc))
-            rr,cc=tc[0]
-            if 0<=rr<R and 0<=cc<C and tc+current[:7] not in SEEN: #(rr,cc) not in [c[0] for c in SEEN]:
-                tentative = H[current]+sum(M[r_][c_] for (r_,c_) in tc)
-                # tentative = H[current]+M[rr][cc]
-                if tc+current[:7] not in H.keys() or tentative<H[tc+current[:7]]:
-                    H[tc+current[:7]]=tentative
-                    Q.append(tc+current[:7])
-        else:
-            rr,cc=r+dr,c+dc
-            if 0<=rr<R and 0<=cc<C and tc+current[:10] not in SEEN: #(rr,cc) not in [c[0] for c in SEEN]:
-                tentative = H[current]+M[rr][cc]
-                if ((rr,cc),)+current[:10] not in H.keys() or tentative<H[((rr,cc),)+current[:10]]:
-                    H[((rr,cc),)+current[:10]]=tentative
-                    Q.append(((rr,cc),)+current[:10])
-            
-    # PATHS.add(current)
-    SEEN.add(current[:11])
 
-    if len(Q)==0:
-        print('fail')
+    if (r,c,dr,dc,n) in SEEN:
+        continue
+    
+    SEEN.add((r,c,dr,dc,n))
+    
+    # if r<0 or r>=R or c<0 or c>=C:
+    #     continue
 
-# for path in PATHS:
-#     if path[0]==goal:
-#         # print(H[path])
-#         print(getcost(path),path)
-#         if getcost(path)==101:
-#             print(getcost(path))
-
-print(min([H[key] for key in H.keys() if key[0]==goal]))
+    if n<3 and (dr,dc) != (0,0):
+        nr = r + dr
+        nc = c + dc
+        if 0<=nr<R and 0<=nc<C:
+            heappush(Q,(hl + M[nr][nc], nr, nc, dr, dc, n+1))
+    for ndr, ndc in [(0,1),(1,0),(0,-1),(-1,0)]:
+        if (ndr,ndc) != (dr,dc) and (ndr,ndc) != (-dr,-dc):
+            nr = r+ndr
+            nc = c+ndc
+            if 0<=nr<R and 0<=nc<C:
+                heappush(Q,(hl + M[nr][nc],nr,nc,ndr,ndc,1))
 
 print('------------------------')
-print('Part 1:',0)
+print('Part 1:',hl)
 print('------------------------')
-print('Part 2:',0)
+
+SEEN=set()
+Q = []
+# PREV = {}
+
+Q.append((0, 0, 0, 0, 0, 0))
+
+while Q:
+    hl, r, c, dr, dc, n = heappop(Q)
+    
+    if r==R-1 and c==C-1 and n>=4:
+        break
+
+    if (r,c,dr,dc,n) in SEEN:
+        continue
+    
+    SEEN.add((r,c,dr,dc,n))
+    
+    # if r<0 or r>=R or c<0 or c>=C:
+    #     continue
+
+    if n<10 and (dr,dc) != (0,0):
+        nr = r + dr
+        nc = c + dc
+        if 0<=nr<R and 0<=nc<C:
+            heappush(Q,(hl + M[nr][nc], nr, nc, dr, dc, n+1))
+    if n>=4 or (dr,dc)==(0,0):
+        for ndr, ndc in [(0,1),(1,0),(0,-1),(-1,0)]:
+            if (ndr,ndc) != (dr,dc) and (ndr,ndc) != (-dr,-dc):
+                nr = r+ndr
+                nc = c+ndc
+                if 0<=nr<R and 0<=nc<C:
+                    heappush(Q,(hl + M[nr][nc],nr,nc,ndr,ndc,1))
+
+print('Part 2:',hl)
 print('------------------------')

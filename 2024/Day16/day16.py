e186c80d50038af8156791e34e2b26c7fbc9bfd2
Peter <peter.sundstrom@hey.com>
2024-12-22 17:41:17 +0100

    day16 part 2 works for examples

--- 2024/Day16/day16.py
+++ 2024/Day16/day16.py
@@ -1,7 +1,8 @@
 print('2024 - Day 16')
 from heapq import heappush, heappop
+from collections import defaultdict
 
-with open('./2024/Day16/input.ex2') as file:
+with open('./2024/Day16/input.ex3') as file:
     grid = [line.rstrip() for line in file]
 
 for r,row in enumerate(grid):
@@ -25,13 +26,14 @@ dr,dc = 0,1
 
 Q.append((0, start[0], start[1], dr, dc))
 
-COST = [[1e30 for _ in range(C)] for __ in range(R)]
-PREV =  {}
+COST = defaultdict(lambda: 1e30) #[[1e30 for _ in range(C)] for __ in range(R)]
+PREV = {}
 
-COST[start[0]][start[1]]=0
+COST[(start[0], start[1], dr, dc)]=0
+COST[start] = 0
 PATH=set()
 
-def printmap(seen=False):
+def printmap(seen=False,visited=set()):
     for r,row in enumerate(grid):
         toprint=''
         for c,column in enumerate(row):
@@ -41,7 +43,9 @@ def printmap(seen=False):
                 toprint+='E'
             elif (r,c) in [(q[1],q[2]) for q in Q]:
                 toprint+='Q'
-            elif seen and (r,c) in [(q[0],q[1]) for q in SEEN]:
+            elif (r,c) in visited:
+                toprint+='0'
+            elif seen and (r,c) in [(q[1],q[2]) for q in SEEN]:
                 toprint+='O'
             elif (r,c) in PATH:
                 toprint+='O'
@@ -52,11 +56,13 @@ def printmap(seen=False):
 LEFT = {(-1,0): (0,-1), (1,0): (0,1), (0,-1): (1,0), (0,1): (-1,0)}
 RIGHT = {(-1,0): (0,1), (1,0): (0,-1), (0,-1): (-1,0), (0,1): (1,0)}
 
-printmap()
+# printmap()
 while Q:
-    cost, r, c, dr, dc = heappop(Q)
+    _, r, c, dr, dc = heappop(Q)
     
     if (r,c) == end:
+        if _ < COST[(r,c)]:
+            COST[(r,c)] = _
         continue
 
     if (r,c,dr,dc) in SEEN:
@@ -64,75 +70,59 @@ while Q:
     
     SEEN.add((r,c,dr,dc))
     
-    for ndr, ndc in [LEFT[(dr,dc)],RIGHT[(dr,dc)],(dr,dc)]:
-        nr = r+ndr
-        nc = c+ndc
-        if grid[nr][nc]=='#': continue
-        # if (ndr,ndc)==(-dr,-dc): continue
-        if 0<=nr<R and 0<=nc<C:
-            if (ndr,ndc)==(dr,dc) and cost + 1 < COST[nr][nc]:
-                COST[nr][nc] = cost + 1
-                PREV[(nr,nc)] = [(r,c)]
-                heappush(Q,(COST[nr][nc],nr,nc,ndr,ndc))
-            if (ndr,ndc)==(dr,dc) and cost + 1 == COST[nr][nc]:
-                if (r,c) not in PREV[(nr,nc)]:
-                    PREV[(nr,nc)].append((r,c))
-                heappush(Q,(COST[nr][nc],nr,nc,ndr,ndc))
-            elif (ndr,ndc) != (dr,dc) and cost + 1001 < COST[nr][nc]:
-                # COST[nr][nc] = COST[r][c] + 1001
-                # PREV[nr][nc] = (r,c)
-                heappush(Q,(COST[r][c]+1000,r,c,ndr,ndc))
-        printmap(seen=True)
-        print(Q)
-        print([COST[r][c] for _,r,c,_,_ in Q])
-        input()
+    # If straight
+    nr = r+dr
+    nc = c+dc
 
+    if (nr,nc) == end:
         pass
 
-# PATHS = [end]
-next = PREV[end]
-PATH = set()
+    if 0<=nr<R and 0<=nc<C and grid[nr][nc]!='#':
+        if COST[(r,c,dr,dc)]+1<COST[(nr,nc,dr,dc)]:
+            COST[(nr,nc,dr,dc)] = COST[(r,c,dr,dc)]+1
+            if COST[(r,c,dr,dc)]+1<COST[(nr,nc)]:
+                COST[(nr,nc)] = COST[(r,c,dr,dc)]+1
+                PREV[(nr,nc)] = [(r,c)] #.append((r,c))
+            heappush(Q,(COST[(nr,nc,dr,dc)],nr,nc,dr,dc))
+        elif COST[(r,c)]+1==COST[(nr,nc,dr,dc)]:
+            PREV[(nr,nc)].append((r,c))
+    for ndr,ndc in [LEFT[(dr,dc)],RIGHT[(dr,dc)]]:
+        if COST[(r,c,dr,dc)]+1000<COST[(r,c,ndr,ndc)]:
+            COST[(r,c,ndr,ndc)] = COST[(r,c,dr,dc)]+1000
+            heappush(Q,(COST[(r,c,ndr,ndc)],r,c,ndr,ndc))
+    pass
 
-PATH.add(end)
-Q = list(PREV[end])
-PREV[start]=[]
-while Q:
-    r,c = heappop(Q)
-    PATH.add((r,c))
-    for nr,nc in PREV[(r,c)]:
-        heappush(Q,(nr,nc))
+def backtrack(source, goal, path=[]):
+    path = path + [source]
+    if source == goal:
+        return [path]
+    if source not in PREV.keys():
+        return [], set()
+    paths = []
+    for p in PREV[source]:
+        if p not in path:
+            newpaths = backtrack(p, goal, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
 
-# print('ALL:', len(ALL))
+paths = backtrack(end, start)
 
-# while next:
-#     if len(next)>1:
-#         pass
-#     for r,c in next:
-#         if (r,c) in [(11, 133),(9, 115),(49, 83),(51, 133),(85, 139),(37, 29),(113, 127),(39, 51),(29, 1),(131, 79),(55, 3),(15, 21),(65, 15),(61, 19),(95, 85),(135, 21),(77, 31),(79, 29)]:
-#             pass
-#         ALL.add((r,c))
-#     next = [x for xs in [PREV[n] for n in next] for x in xs if x!=start]
+visited=set()
 
-print('visited:',len(PATH))
+for path in paths:
+    print(path[0],path[-1])
+    visited.update(path)
 
-for k,p in PREV.items():
-    if len(p)>1:
-        print(k,p)
-# print(PREV.items())
-# PATH.append(start)
+print(PREV[end])
 
-printmap()
-# print(len(PATH))
+print(COST[end])
 
-# for r,row in enumerate(COST):
-#     for c,cost in enumerate(row):
-#         if 0<COST[end[0]][end[1]]-cost<10:
-#             print(r,c,cost)
-
-print(COST[end[0]][end[1]])
+printmap(visited=visited)
+# 582 is too high
 
 print('------------------------')
-print('Part 1:',0)
+print('Part 1:',COST[end])
 print('------------------------')
-print('Part 2:',0)
+print('Part 2:',len(visited))
 print('------------------------')
